023c23ef6cc58e120d1e501bef502ca511d91f6a
Tomás Lara <tmslara.a@gmail.com>
2019-05-10 14:46:45 -0400

    cambios implementacion
    *Se agrega funcion que calcule el delay.
    *Se agrega factor dos para el calculo de pulsos.

--- Codigos motor/motor 85bygh450d 008/ejemplo_movimiento/ejemplo_movimiento.ino
+++ Codigos motor/motor 85bygh450d 008/ejemplo_movimiento/ejemplo_movimiento.ino
@@ -9,15 +9,17 @@
   B- blue
 */
 
+//PIN's DEFINITION.
 const int enable_1 = 9;        // enable - driver stepper 1
 const int direction_1 = 10;    // direction - driver stepper 1
 const int pulse_1 = 11;        // pulse - driver stepper 1
 
+//FIXED VARIABLES.
+
 //Pulse per revolution
-const float pulse_per_rev = 10000.0;
+float pulse_per_rev = 10000.0;
 float rps = 0.10;
-float delay_1;
-long delay_1_long;
+long pulseDelay;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -31,17 +33,32 @@ void setup() {
   digitalWrite(direction_1, HIGH);
 
   // delay
-  delay_1 = 1000.0/(pulse_per_rev*rps);
-  delay_1_long = (long) delay_1;
-
-  Serial.print(delay_1);
-  Serial.print(delay_1_long);
+  pulseDelay = delayValue(pulse_per_rev,rps);
+  Serial.print(pulseDelay);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(pulse_1, HIGH);   // turn the pulse_1 on (HIGH is the voltage level)
-  delay(delay_1_long);                // wait for a delay 1 time
+  delay(pulseDelay);                // wait for a delay 1 time
   digitalWrite(pulse_1, LOW);    // turn the pulse_1 off by making the voltage LOW
-  delay(delay_1_long);                // wait for a delat 1 time
+  delay(pulseDelay);                // wait for a delat 1 time
+}
+
+
+//FUNCTIONS
+//Function that calculates the delay value for a certain number of rps.
+long delayValue (float pulses_per_revolution, float rps){
+  // Variables definition
+  long delays;
+  long delayms;
+  float pulses_per_second;
+    
+  //pulses per second.
+  pulses_per_second = pulses_per_revolution * rps;
+  //duration of values LOW or HIGH.
+  delays = (long) 1 / (2*pulses_per_second);
+  //conversion to miliseconds
+  delayms = delays * 1000;
+  return delayms;
 }
